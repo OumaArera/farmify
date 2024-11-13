5367b63f099ee78d2b893a4deca041e98efa1882
OumaArera <johnouma999@gmail.com>
2024-11-13 20:43:57 +0300

    feat: basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import MyAccount from './components/MyAccount';
+import ItemDetails from './components/ItemDetails';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Checkout from './components/Checkout';
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello, Farmify!</div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/myaccount" element={<MyAccount />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/items/:id" element={<ItemDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
